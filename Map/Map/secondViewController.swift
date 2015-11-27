4672fa66f4d74ec2c01eff3346171bf0c97e9a75
LeeDoona(이두나) <nesk93@naver.com>
2015-11-27 20:18:26 +0900

    move to mapView

--- Map/Map/secondViewController.swift
+++ Map/Map/secondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  secondViewController.swift
 //  Map
 //
 //  Created by Doona on 2015. 11. 27..
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class secondViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
